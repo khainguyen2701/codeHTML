7c807947a0604aa78d8c8d58cc7cbdd58b2b2fe0
khainguyen2701 <nguyenvankhai2701@gmail.com>
2021-07-06 15:01:14 +0700

    Edit From HTML local new file

--- js/main.js
+++ js/main.js
@@ -4,3 +4,14 @@ $(document).ready(function(){
         $('#sidebar').toggleClass('show');
     })
 })
+window.onscroll = function() {scrollFunction()};
+function scrollFunction()
+{
+    if(document.body.scrollTop>200|| document.documentElement.scrollTop > 50)
+    {
+        document.querySelector('header').style.background='rgba(26, 119, 71, 0.4)';
+    }
+    else {
+        document.querySelector('header').style.background='transparent';
+      }
+}
