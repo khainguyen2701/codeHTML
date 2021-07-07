bcd93e00da44ddec14db79248848acfe357325b7
khainguyen2701 <nguyenvankhai2701@gmail.com>
2021-07-07 16:26:15 +0700

    Upload 07/07/2021

--- js/main.js
+++ js/main.js
@@ -15,3 +15,25 @@ function scrollFunction()
         document.querySelector('header').style.background='transparent';
       }
 }
+
+var $ = document.querySelector.bind(document);
+var $$ = document.querySelectorAll.bind(document);
+const tabs = $$('.tab-item');
+const line = $('.tabs .line');
+const panes = $$('.tab-pane');
+const tabActive = $('.tab-item.active');
+line.style.left = tabActive.offsetLeft + 'px';
+line.style.width = tabActive.offsetWidth + 'px';
+
+tabs.forEach(function(tab,index){
+    const pane = panes[index];
+    tab.onclick = function()
+    {
+        $(".tab-item.active").classList.remove("active");
+        $(".tab-pane.active").classList.remove("active");
+        line.style.left = this.offsetLeft + 'px';
+        line.style.width = this.offsetWidth + 'px';
+        this.classList.add("active");
+        pane.classList.add("active");
+    }
+})
